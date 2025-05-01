dc24250fd6414ebb2d1969aecc9e4a54168c4272
pkkrsingh877 <pkkrsingh877@gmail.com>
2025-05-01 16:21:04 +0530

    Profile viewing & updating possible.

--- models/User.ts
+++ models/User.ts
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema({
-    id: { type: String, required: true, unique: true },
     name: { type: String, required: true },
     email: { type: String, required: true, unique: true },
     image: { type: String },
